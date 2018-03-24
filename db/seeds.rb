5d9c3a78b122604a309563edc0376f1b5769cc05
Steve Brewer <steve.brewer@futurelearn.com>
2018-03-24 15:48:23 +0000

    Add organiser seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,58 @@
-Organiser.create(name: 'Jerome Clarke', image: 'https://pbs.twimg.com/profile_images/971161748823781376/y1W6FusA_400x400.jpg', role: 'Co-founder', title: 'Associate iOS Developer', linkedin: 'jeromejclarke', twitter: 'jeromejjclarke', github: 'JeromeJJClarke')
+Organiser.create(
+  name: 'Ali Hamdan',
+  image: 'https://media.licdn.com/dms/image/C5603AQEUIndFvh_Acg/profile-displayphoto-shrink_800_800/0?e=1527091200&v=alpha&t=6EfOh6OszYP3o3O6XoMiQcK1t8NwMbYLSLqNPwBvkZQ',
+  role: 'Organiser',
+  title: 'Network Security Engineer',
+  linkedin: 'ali-hamdan-ba8571b3',
+  github: 'yxorp'
+)
+
+Organiser.create(
+  name: 'Atila Keri',
+  image: 'https://media.licdn.com/dms/image/C4E03AQHKPP4TsGoV7w/profile-displayphoto-shrink_800_800/0?e=1527091200&v=alpha&t=LjIuZl51gfALkSmRNfIYE48_JmhK1zjW0qtONbwN470',
+  role: 'Organiser',
+  title: 'Software Developer',
+  linkedin: 'jeromejclarke',
+  twitter: 'jeromejjclarke',
+  github: 'JeromeJJClarke'
+)
+
+Organiser.create(
+  name: 'Gary Siu',
+  image: 'https://pbs.twimg.com/profile_images/971161748823781376/y1W6FusA_400x400.jpg',
+  role: 'Organiser',
+  title: 'Software Developer',
+  linkedin: 'jeromejclarke',
+  twitter: 'jeromejjclarke',
+  github: 'JeromeJJClarke'
+)
+
+Organiser.create(
+  name: 'Jerome Clarke',
+  image: 'https://pbs.twimg.com/profile_images/971161748823781376/y1W6FusA_400x400.jpg',
+  role: 'Co-founder',
+  title: 'Associate iOS Developer',
+  linkedin: 'jeromejclarke',
+  twitter: 'jeromejjclarke',
+  github: 'JeromeJJClarke'
+)
+
+Organiser.create(
+  name: 'Sharla Kew',
+  image: 'https://media.licdn.com/dms/image/C5603AQF9ajpE3z1O5w/profile-displayphoto-shrink_800_800/0?e=1527091200&v=alpha&t=sffNAXRxeFD8jOvzsQuhix9Bo3QGS57dpaQ3pYe0FF8',
+  role: 'Organiser',
+  title: 'Software Engineer',
+  linkedin: 'jeromejclarke',
+  twitter: 'jeromejjclarke',
+  github: 'JeromeJJClarke'
+)
+
+Organiser.create(
+  name: 'Steve Brewer',
+  image: 'https://pbs.twimg.com/profile_images/977573143199010817/tPUNXk5g_400x400.jpg',
+  role: 'Co-founder',
+  title: 'Ruby developer',
+  linkedin: 'svpersteve',
+  twitter: 'svpersteve',
+  github: 'svpersteve'
+)
