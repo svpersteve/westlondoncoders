10c73b35bd125929610f0ab8eb82b3896431b448
Steve Brewer <steve.brewer@futurelearn.com>
2018-03-24 15:03:53 +0000

    Join Slack page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   resources :slack_invitations, only: %i[new create], path: 'join-slack'
+  get '/join-slack', to: 'pages#join_slack', as: 'join_slack'
 end
