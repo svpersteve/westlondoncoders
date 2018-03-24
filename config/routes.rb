1df1dd57f1a747010b4b54ff09436f96ce51046f
Steve Brewer <steve.brewer@futurelearn.com>
2018-03-24 15:04:02 +0000

    Information page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :slack_invitations, only: %i[new create], path: 'join-slack'
   get '/join-slack', to: 'pages#join_slack', as: 'join_slack'
+  get '/information', to: 'pages#information', as: 'information'
 end
